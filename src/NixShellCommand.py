dfe1012f25118cbe82411679d66361ab9ecc35b8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-10 08:04:20 +0100

    another attempt

--- src/NixShellCommand.py
+++ src/NixShellCommand.py
@@ -12,48 +12,42 @@ class NixShellReloadCommand(sublime_plugin.WindowCommand):
     if not dirs:
       print("No dirs passed to nix_shell plugin, exiting here.")
       return
-    shellFile = None
+    shellFile   = None
     curDir = Path(dirs[0])
     print("Beginning search for shell.nix in " + str(curDir))
     shellFile = curDir.joinpath('shell.nix')
-    # while shellFile is None:
-    #   candidate = curDir.joinpath('shell.nix')
-    #   if candidate.exists():
-    #     shellFile = candidate
-    #     print("Found shell file: " + str(shellFile)
-    #   else:
-    #     print("did not find shell file")
-    #     break
-    #     # parent = candidate.parent
-    #     # print(parent)
-    #     # if parent == candidate:
-    #     #     break # We're at root
-    #     # else:
-    #     #     curDir = candidate.parent
     print(shellFile)
     if not shellFile.exists():
         print("No shell file found at " + str(shellFile))
         return
     bashPath = distutils.spawn.find_executable("bash")
     nixShellPath = distutils.spawn.find_executable("nix-shell")
-    print(nixShellPath)
+    print("nix-shell found at " + nixShellPath)
+    gitPath = distutils.spawn.find_executable("git")
+    print("git found at " + gitPath)
+    bashPath = distutils.spawn.find_executable("bash")
+    print("bash found at " + bashPath)
     args = [nixShellPath, "--command", "echo $PATH"]
     print("running " + ' '.join(args))
     print("in " + str(shellFile.parent))
-    print(sys.version_info)
+    print ("Old path:")
+    print(os.environ['PATH'])
     # output = subprocess.check_output("nix-shell", shell=True)
     # print(output)
     process = subprocess.Popen(args,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE,
-                     env = {}, # To make sure LD_PRELOAD doesn't get in the way.
+                     # We empty the path to make sure LD_PRELOAD doesn't get in
+                     # the way.  But we also need to make sure that git is
+                     # there otherwise the `builtins.fetchGit` doesn't work.
+                     env = { "PATH": str(Path(gitPath).parent) + ":" + str(Path(bashPath).parent) },
                      cwd = str(shellFile.parent))
     stdout, stderr = process.communicate()
     print ("code: " + str(process.returncode))
     print ("stdout: " + str(stdout))
     print ("stderr: " + str(stderr))
     # print ("currently in path:" + os.environ['PATH'])
-    new_path=str(stdout) + ":" + str(Path(nixShellPath).parent) + ":" + str(Path(bashPath).parent)
+    new_path=str(stdout) + ":" + str(Path(nixShellPath).parent) + ":" + str(Path(bashPath).parent) + ":" + os.environ['PATH']
     os.environ['PATH'] = new_path
     print ("New path:")
     print(os.environ['PATH'])
